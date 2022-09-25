a0e169e54fc1a8ce837cbf72853244d61efb3fcc
Julian Geppert <spctstr@gmail.com>
2022-09-25 23:21:18 +0200

    chapter 1 exercise: support html entities &lt; and &gt;

--- browser.py
+++ browser.py
@@ -83,6 +83,7 @@ def show(body):
   tag_name = ""
   in_body = False
   in_angle = False
+  output = ""
   for c in body:
     if c == "<":
       in_angle = True
@@ -93,9 +94,11 @@ def show(body):
         in_body = not in_body
     elif in_angle:
       tag_name += c
-    elif in_body and not in_angle:
-      print(c, end="")
-  print("")
+    elif in_body:
+      output += c
+  output = output.replace("&lt;", "<")
+  output = output.replace("&gt;", ">")
+  print(output)
 
 
 def load(url):
